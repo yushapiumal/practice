638f86810a11c90c9b48fea5bd9793982dd695c7
yushapiumal <adypiumalhpbk0701@gmail.com>
2023-12-06 23:24:25 -0800

    connect database in php

---
+++ connect_database.php
@@ -0,0 +1,12 @@
+<?php
+
+//connect database
+$dbServerName ="localhost";
+$dbUserName = "root";
+$dbPassWord = "";
+$dbName = "php_tutorial";
+
+
+$connect = mysqli_connect($dbServerName,$dbUserName,$dbPassWord,$dbName)
+
+?>
---
+++ select_unique_data.php
@@ -0,0 +1,32 @@
+<?php
+include_once 'connect_database.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+
+</body>
+
+<?php
+// access data
+$sql="SELECT * FROM users WHERE lastName='hasaranga';";
+$result = mysqli_query($connect,$sql); //add variable in  connected database and selected table
+$check_result = mysqli_num_rows($result); // save num of rows in database for variable
+
+// output data
+if($check_result>0) // check have arows in database
+{
+    while($row = mysqli_fetch_assoc($result))
+    {
+     echo $row['ID']."<br>";
+     echo $row['lastName']."<br>";
+    }
+}
+?>
+
+</html>
